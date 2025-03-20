9174cede799a7c0e04d9987bec45336ca5e2c902
ormf <junk14@selma.hfmdk-frankfurt.de>
2025-03-20 17:41:57 +0100

    removed ~ from post-install.sh

--- work/install/post-install.sh
+++ work/install/post-install.sh
@@ -3,7 +3,7 @@
 # sudo mkdir -p /usr/local/lib/systemd/user-environment-generators
 sudo ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 cd /home/admin/work/install/scripts
-sudo /home/admin/work/install/scripts/user1-home.sh
+sudo -u gast /home/admin/work/install/scripts/user1-home.sh
 # sudo cp environment /etc/
 # sudo chown root.root /etc/environment
 # sudo chmod 644 /etc/environment
@@ -14,14 +14,14 @@ sudo /home/admin/work/install/scripts/user1-home.sh
 # sudo chmod a+x /usr/local/bin/pulse-jack-post-start.sh
 # sudo chmod a+x /usr/local/bin/pulse-jack-pre-stop.sh
 cd /home/admin/work/install/config
-sudo mkdir -p ~gast/.themes
-sudo mkdir -p ~admin/.themes
-sudo cp -R Adapta-Nokto ~gast/.themes/
-sudo chmod 755 ~gast/.themes
-sudo chown -R gast:gast ~gast/.themes
-sudo chmod 755 ~admin/.themes
-sudo cp -R Adapta-Nokto ~admin/.themes/
-sudo chown -R admin:admin ~admin/.themes
+sudo mkdir -p /home/gast/.themes
+sudo mkdir -p /home/admin/.themes
+sudo cp -R Adapta-Nokto /home/gast/.themes/
+sudo chmod 755 /home/gast/.themes
+sudo chown -R gast:gast /home/gast/.themes
+sudo chmod 755 /home/admin/.themes
+sudo cp -R Adapta-Nokto /home/admin/.themes/
+sudo chown -R admin:admin /home/admin/.themes
 sudo ln -s /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 sudo timedatectl set-local-rtc 0
 sudo locale-gen
--- work/install/scripts/user1-home.sh
+++ work/install/scripts/user1-home.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-su -l gast
+# su -l gast
 rsync -av /home/gast//user1-home/* /home/gast/
 rsync -av /home/gast/user1-home/\.* /home/gast/
 mv /home/gast/local-projects /home/gast/quicklisp/
 rm -rf user1-home
-exit
+# exit
 
