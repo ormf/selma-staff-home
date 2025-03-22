3f0910251a8382533eb555cf482d8e66f4fa95c5
ormf <junk14@selma.hfmdk-frankfurt.de>
2025-03-22 17:52:56 +0100

    added git clone commands to user1-home.sh

--- work/install/scripts/user1-home.sh
+++ work/install/scripts/user1-home.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # su -l gast
+sudo -u gast git clone https://github.com/ormf/cm.git /home/gast/local-projects/cm
+sudo -u gast git clone https://github.com/ormf/fomus.git /home/gast/local-projects/fomus
+sudo -u gast git clone https://github.com/titola/incudine.git /home/gast/local-projects/incudine
+sudo -u gast git clone https://github.com/ormf/cellctl.git /home/gast/local-projects/cellctl
+sudo -u gast git clone https://github.com/ormf/cl-orgel-gui.git /home/gast/local-projects/cl-orgel-gui
 sudo -u gast rsync -av /home/gast//user1-home/* /home/gast/
 sudo -u gast rsync -av /home/gast/user1-home/\.* /home/gast/
 sudo -u gast mv /home/gast/local-projects /home/gast/quicklisp/
